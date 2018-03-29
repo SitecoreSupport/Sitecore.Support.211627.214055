bb16fba1550ce72d0d917ee9536a493828129605
DK\mao <mao@sitecore.net>
2018-03-29 16:58:03 +0300

    Add event handler to clear cloned items caches when saving source item.

---
+++ src/Sitecore.Support.211627.214055/LanguageFallbackFieldValuesCache/ItemEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Sitecore.Caching;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Events;
+
+namespace Sitecore.Support.LanguageFallbackFieldValuesCache
+{
+  public class ItemEventHandler
+  {
+    public void OnItemSaved(object sender, EventArgs args)
+    {
+      try
+      {
+        Item item = Event.ExtractParameter(args, 0) as Item;
+        Assert.IsNotNull(item, "No item in parameters");
+        var fallbackFieldValuesCache = item.Database.Caches.FallbackFieldValuesCache as LanguageFallbackIndexedFieldValuesCache;
+        var clearCacheMethod = typeof(LanguageFallbackIndexedFieldValuesCache)
+          .GetMethod("ClearCache", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        // Clear caches for all clones of item passed as args.
+        foreach (var link in Globals.LinkDatabase.GetItemReferrers(item, false))
+        {
+          var referrer = Configuration.Factory.GetDatabase(link.SourceDatabaseName).GetItem(link.SourceItemID);
+          if (referrer.Source != null && referrer.Source.ID == item.ID)
+          {
+            clearCacheMethod.Invoke(fallbackFieldValuesCache, new object[] { referrer });
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Log.Error($"[Sitecore.Support.211627.214055] Error occurred while handling event: {Environment.StackTrace}", this);
+      }
+     
+    }
+  }
+}
